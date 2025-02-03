92d59cd84d0869d9441fe98d9dff1171c90545fd
Brown_ Alexander James <alexander.brown@login-p3n01.kamiak.wsu.edu>
2025-02-03 11:55:53 -0800

    feat: practice code to fix

---
+++ src/app/hello_lab.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    lab_members = ["Adlxenaer", "Ineer", "Rcradio"]
+
+    print("The Seifert lab has the following members:")
+    for member in lab_members:
+        print(f"Lab member: {member}")
